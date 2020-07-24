9b70f978e3ce0531eb9d01666419c2db91d97b0b
Erik Tollerud <erik.tollerud@gmail.com>
2020-07-24 14:39:30 -0400

    Add prep file

--- 2020a_prepare.py
+++ 2020a_prepare.py
@@ -10,7 +10,10 @@ sciarcs = [
 (base+'mbxgpP202005290034.fits', base+'mbxgpP202005290035.fits'), # 134-pgc66318-188048484
 #(base+'mbxgpP202005210063.fits', base+'.fits'), # 091-pgc66318-188047679   ... focus did not go back properly after a move and it drifted. We didn't notice. Reject.
 (base+'mbxgpP202005210064.fits', base+'mbxgpP202005210065.fits'), # 040-pgc67782-247125749
-#(base+'mbxgpP202005190077.fits', base+'mbxgpP202005190078.fits'), # 017-pgc66318-192117557
+(base+'mbxgpP202007190052.fits', base+'mbxgpP202007190053.fits'), # 172-pgc66318-188071178
+(base+'mbxgpP202007180066.fits', base+'mbxgpP202007180067.fits'), # 154-pgc67146-212645129
+(base+'mbxgpP202007170082.fits', base+'mbxgpP202007170083.fits'), # 093-pgc66318-188043593
+(base+'mbxgpP202006240064.fits', base+'mbxgpP202006240065.fits'), # 208-pgc66934-201237793
 ]
 
 saga_salt.prepare_reduction(sciarcs, clobber=True, basepath='2020a', pathtoarcs='/home/pysalt/linelists/')
